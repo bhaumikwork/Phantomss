4aa3df748009d4df053d14162707afac2291b082
bhaumik gadani <bhaumikwork@gmail.com>
2016-03-25 18:36:07 +0530

    port changed.

--- web.js
+++ web.js
@@ -85,7 +85,7 @@ app.post('/screenshot', function(request, response) {
 });
 
 
-var port = 4000; //process.env.PORT || 5000;
+var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
