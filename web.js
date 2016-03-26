85e0436f39eaf0621ad0f0c9a2f02bd4d719c5af
bhaumik gadani <bhaumikwork@gmail.com>
2016-03-26 11:43:09 +0530

    map.html added to node.

--- web.js
+++ web.js
@@ -6,7 +6,8 @@ var childProcess = require('child_process');
 var guid = require('guid');
 var AWS = require('aws-sdk');
 var fs = require('fs');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+var url = require('url');
 
 var _ServerConfig = require('./config.json');
 
@@ -25,20 +26,19 @@ var app = express();
 app.use(bodyParser.json());
 
 app.get('/', function(req, res){
-  res.send('<html><head><title>Screenshots!</title></head><body><h1>Screenshots!</h1><form action="/screenshot" method="POST">URL: <input name="address" value="" placeholder="http://"><br>Size:<input name="size" value="" placeholder="1024px or 1024px*1000px"><br>Zoom factor:<input name="zoom" value="1"><br><input type="hidden" name="redirect" value="true"><input type="submit" value="Get Screenshot!"></form></body></html>');
+  var url_parts = url.parse(req.url, true);
+  var params = url_parts.query;
+  var pageContent = fs.readFileSync('./map.html').toString()  ;
+  res.end(pageContent.replace("###START###",params.start).replace("###END###",params.end));
 });
 
 app.post('/screenshot', function(request, response) {
-  console.log("request arrived with "+request.body.address);
-  if(!request.body.address) {
-    return response.status(400).json({ 'error': 'You need to provide the website address.' });
-  }
-
+  var address = _ServerConfig.host + "?start=" + request.body.start + "&end=" + request.body.end;
   var filename = guid.raw() + '.png';
   var filenameFull = './images/' + filename;
   var childArgs = [
     __dirname + '/rasterize.js',
-    format(request.body.address),
+    format(address),
     filenameFull,
     _ServerConfig.size,
     _ServerConfig.zoom
@@ -46,7 +46,7 @@ app.post('/screenshot', function(request, response) {
 
   //grap the screen
   childProcess.execFile('phantomjs', childArgs, function(error, stdout, stderr){
-    console.log("Grabbing screen for: " + request.body.address);
+    console.log("Grabbing screen for: " + address);
     if(error !== null) {
       console.log("Error capturing page: " + error.message + "\n for address: " + childArgs[1]);
       return response.status(500).json({ 'error': 'Problem capturing page.' });
